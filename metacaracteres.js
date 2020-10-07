e6a504a8671bfad5bfb5e1e4fc6561fd43d12dc5
Leo Porto <leonardoreisporto@gmail.com>
2020-10-07 16:23:33 -0300

    entendendo valores literais VS metacaracteres

--- metacaracteres.js
+++ metacaracteres.js
@@ -1,2 +1,24 @@
 // . ? * + - ^ $ | ( ) { } [ ] \ :
 
+const texto = '1,2,3,4,5,6,a.b c!d?e'
+
+
+//como o . é um metacaractere, usamos a \ para indicar que 
+//queremos usar o seu valor literal e nao sua funcao de metacaractere
+const ponto = /\./g
+
+console.log(texto.match(ponto))
+
+
+//usando o ponto para splitar a string
+console.log(texto.split(ponto))
+
+
+//sempre que precisar usar o valor literal de
+//um caractere que tem funcao de metacaractere,
+//usamos contrabarra \
+const simbolos = /,|\.|5|\?|\!/g
+
+console.log(texto.match(simbolos))
+
+console.log(texto.split(simbolos))
