b7748cd311572324ef48fa16a444f56804fc777b
Leo Porto <leonardoreisporto@gmail.com>
2020-10-07 15:44:10 -0300

    usando a flag g para buscar a palavra globalmente no texto, todas as ocorrencias

--- helloRegex.js
+++ helloRegex.js
@@ -1,6 +1,6 @@
 const texto = 'O cadastro das chaves do Pix já foi liberado, embora as transações com o novo sistema de pagamentos do Banco Central só estarem disponíveis a partir de 16 de novembro de 2020. Contudo, mesmo nesse primeiro momento, é preciso estar atento às fraudes e roubo de dados que aproveitam da novidade. Saiba como cadastrar as chaves do Pix com segurança.'
 
 
-const regex = /Pix/
+const regex = /Pix/g
 
 console.log(texto.match(regex))
